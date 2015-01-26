d40e538f594e7d002bf156a9e412cc4715b550c5
loordgek <stefanhempenius@hotmail.com>
2015-01-26 22:16:26 +0100

    update

--- src/main/java/com/minemaarten/advancedmod/AdvancedMod.java
+++ src/main/java/com/minemaarten/advancedmod/AdvancedMod.java
@@ -1,15 +1,20 @@
 package com.minemaarten.advancedmod;
 
 import com.minemaarten.advancedmod.init.ModBlocks;
+import com.minemaarten.advancedmod.init.ModTileEntities;
+import com.minemaarten.advancedmod.network.DescriptionHandler;
+import com.minemaarten.advancedmod.network.NetworkHandler;
 import com.minemaarten.advancedmod.proxy.CommonProxy;
 import com.minemaarten.advancedmod.reference.Reference;
 import com.minemaarten.advancedmod.utility.Log;
+import com.minemaarten.advancedmod.world.gen.WorldGeneratorFlag;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class AdvancedMod{
@@ -22,7 +27,11 @@ public class AdvancedMod{
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
         ModBlocks.init();
+        ModTileEntities.init();
         proxy.preInit();
+        GameRegistry.registerWorldGenerator(new WorldGeneratorFlag(), 0);
+        NetworkHandler.init();
+        DescriptionHandler.init();
         Log.info("Pre Initialization Complete!");
     }
 
--- src/main/java/com/minemaarten/advancedmod/proxy/ClientProxy.java
+++ src/main/java/com/minemaarten/advancedmod/proxy/ClientProxy.java
@@ -1,10 +1,26 @@
 package com.minemaarten.advancedmod.proxy;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+
+import com.minemaarten.advancedmod.client.KeyInputHandler;
+import com.minemaarten.advancedmod.client.Keybindings;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+
 public class ClientProxy extends CommonProxy{
 
     @Override
     public void preInit(){
+        registerKeybinds();
+    }
 
+    private void registerKeybinds(){
+        FMLCommonHandler.instance().bus().register(new KeyInputHandler());
+        for(Keybindings key : Keybindings.values()) {
+            ClientRegistry.registerKeyBinding(key.getKeybind());
+        }
     }
 
     @Override
@@ -15,6 +31,11 @@ public class ClientProxy extends CommonProxy{
     @Override
     public void postInit(){
 
+    }
+
+    @Override
+    public EntityPlayer getClientPlayer(){
+        return Minecraft.getMinecraft().thePlayer;
     }
 
 }
