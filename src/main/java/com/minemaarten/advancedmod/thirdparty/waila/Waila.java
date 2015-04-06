21ce31c306b68af2d4b61598e0fd3bb1a046e069
loordgek <stefanhempenius@hotmail.com>
2015-04-06 15:01:29 +0200

    lol

--- src/main/java/com/minemaarten/advancedmod/thirdparty/waila/Waila.java
+++ src/main/java/com/minemaarten/advancedmod/thirdparty/waila/Waila.java
@@ -1,13 +1,17 @@
 package com.minemaarten.advancedmod.thirdparty.waila;
 
+import cpw.mods.fml.common.Loader;
 import mcp.mobius.waila.api.IWailaRegistrar;
 
 import com.minemaarten.advancedmod.block.BlockCamoMine;
 
 public class Waila{
+
     public static void onWailaCall(IWailaRegistrar registrar){
-        registrar.registerStackProvider(new WailaCamoHandler(), BlockCamoMine.class);
-        registrar.registerBodyProvider(new WailaCamoHandler(), BlockCamoMine.class);
-        registrar.registerNBTProvider(new WailaCamoHandler(), BlockCamoMine.class);
+        if (Loader.isModLoaded("Waila")){
+            registrar.registerStackProvider(new WailaCamoHandler(), BlockCamoMine.class);
+            registrar.registerBodyProvider(new WailaCamoHandler(), BlockCamoMine.class);
+            registrar.registerNBTProvider(new WailaCamoHandler(), BlockCamoMine.class);
+        }
     }
 }
--- src/main/java/com/minemaarten/advancedmod/thirdparty/waila/WailaCamoHandler.java
+++ src/main/java/com/minemaarten/advancedmod/thirdparty/waila/WailaCamoHandler.java
@@ -17,36 +17,38 @@ import net.minecraft.world.World;
 import com.minemaarten.advancedmod.init.ModBlocks;
 import com.minemaarten.advancedmod.tileentity.TileEntityCamoMine;
 
-public class WailaCamoHandler implements IWailaDataProvider{
+public class WailaCamoHandler implements IWailaDataProvider {
 
-    @Override
-    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config){
-        TileEntityCamoMine te = (TileEntityCamoMine)accessor.getTileEntity();
+        @Override
+        public ItemStack getWailaStack (IWailaDataAccessor accessor, IWailaConfigHandler config){
+        TileEntityCamoMine te = (TileEntityCamoMine) accessor.getTileEntity();
         int side = accessor.getSide().ordinal();
         ItemStack stack = te.getCamouflage(side);
-        if(stack != null && stack.getItem() instanceof ItemBlock) {
+        if (stack != null && stack.getItem() instanceof ItemBlock) {
             return stack;
         } else {
             return new ItemStack(ModBlocks.camoMine);
         }
     }
 
-    @Override
-    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config){
+        @Override
+        public List<String> getWailaHead (ItemStack itemStack, List < String > currenttip, IWailaDataAccessor
+        accessor, IWailaConfigHandler config){
         return currenttip;
     }
 
-    @Override
-    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config){
+        @Override
+        public List<String> getWailaBody (ItemStack itemStack, List < String > currenttip, IWailaDataAccessor
+        accessor, IWailaConfigHandler config){
         NBTTagCompound tag = accessor.getNBTData();
         String target = tag.getString("target");
-        if(!target.equals("")) {
+        if (!target.equals("")) {
             currenttip.add(I18n.format("advancedMod.waila.camoMine.target") + " " + EnumChatFormatting.GREEN + target);
         }
         int timer = tag.getInteger("timer");
-        if(timer == 0) {
+        if (timer == 0) {
             currenttip.add(I18n.format("advancedMod.waila.camoMine.primed"));
-        } else if(timer == -1) {
+        } else if (timer == -1) {
             currenttip.add(I18n.format("advancedMod.waila.camoMine.notPrimed"));
         } else {
             currenttip.add(I18n.format("advancedMod.waila.camoMine.primingIn") + " " + timer);
@@ -54,17 +56,20 @@ public class WailaCamoHandler implements IWailaDataProvider{
         return currenttip;
     }
 
-    @Override
-    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config){
+        @Override
+        public List<String> getWailaTail (ItemStack itemStack, List < String > currenttip, IWailaDataAccessor
+        accessor, IWailaConfigHandler config){
         return currenttip;
     }
 
-    @Override
-    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x, int y, int z){
-        TileEntityCamoMine tile = (TileEntityCamoMine)te;
+        @Override
+        public NBTTagCompound getNBTData (EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world,int x,
+        int y, int z){
+        TileEntityCamoMine tile = (TileEntityCamoMine) te;
         tag.setString("target", tile.getTarget());
         tag.setInteger("timer", tile.getTimer());
         return tag;
     }
+
 
 }
